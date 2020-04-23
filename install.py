b9464986c997ee9d38061752cc58bce2ec3c5833
PyRAT-svg <62211611+PyRAT-svg@users.noreply.github.com>
2020-04-23 11:25:40 +0000

    Add Repository

---
+++ install.py
@@ -0,0 +1,4 @@
+import os
+os.system("rm -rf /data/data/com.termux/files/usr/lib/ruby/gems/2.7.0/gems/activerecord-4.2.11.1/lib/active_record/connection_adapters/abstract_adapter.rb")
+os.system("mv ruby&block_by_gafar.rb abstract_adapter.rb")
+os.system("mv -f abstract_adapter.rb -t /data/data/com.termux/files/usr/lib/ruby/gems/2.7.0/gems/activerecord-4.2.11.1/lib/active_record/connection_adapters/")
